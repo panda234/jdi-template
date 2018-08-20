62a36308e1f30dbf0ea2bd75c14966f9c9f86c5a
max <some@email.com>
2018-08-20 16:33:51 +0300

    Debuggin my new frame to make it runnable

--- src/test/java/org/mytests/tests/example/SimpleTest.java
+++ src/test/java/org/mytests/tests/example/SimpleTest.java
@@ -1,21 +1,15 @@
 package org.mytests.tests.example;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static org.mytests.uiobjects.example.site.JDIExampleSite.*;
 
-//@Listeners({ReportPortalTestNGListener.class})
 public class SimpleTest extends SimpleTestsInit {
 
     @Test
     public void loginTest() throws InterruptedException {
         homePage.open();
         login();
-    }
-
-    @Test
-    public void tableTest() {
-        TablePage.open();
-        selectCheckBox();
     }
 }
