65572126c460a0419dc78fb69c21525604644641
max <some@email.com>
2018-08-21 13:21:21 +0300

    Set up data provider

--- src/test/java/org/mytests/tests/example/SimpleTest.java
+++ src/test/java/org/mytests/tests/example/SimpleTest.java
@@ -2,14 +2,22 @@ package org.mytests.tests.example;
 
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.mytests.uiobjects.example.site.JDIExampleSite.convertMoney;
 import static org.mytests.uiobjects.example.site.JDIExampleSite.convertPage;
 
 public class SimpleTest extends SimpleTestsInit {
 
-    @Test
-    public void loginTest() throws InterruptedException {
+    @Test(dataProvider = "TestData")
+    public void loginTest(HashMap<String, String> data) throws InterruptedException {
         convertPage.open();
-        convertMoney("1000", "IRR иранский риал", "HKD гонконгский доллар");
+        convertMoney("1000", "RUB российский рубль", getValueFromMap(data));
+    }
+
+    private String getValueFromMap(HashMap<String, String> data) {
+        Map.Entry<String, String> entry = data.entrySet().iterator().next();
+        return entry.getKey() + " " + entry.getValue();
     }
 }
--- src/test/java/org/mytests/tests/example/SimpleTestsInit.java
+++ src/test/java/org/mytests/tests/example/SimpleTestsInit.java
@@ -3,60 +3,74 @@ package org.mytests.tests.example;
 import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
 import org.mytests.uiobjects.example.site.JDIExampleSite;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 
 import java.sql.*;
 import java.util.HashMap;
-import java.util.Map;
 
 import static com.epam.jdi.uitests.core.settings.JDISettings.logger;
 
+
 public class SimpleTestsInit extends TestNGBase {
+
+    private static ResultSet resultSet;
+
+    private Statement stmt = null;
+
+    private Connection conn = null;
+
     @BeforeSuite(alwaysRun = true)
     public static void setUp() {
-       WebSite.init(JDIExampleSite.class);
+        WebSite.init(JDIExampleSite.class);
         logger.info("Run Tests");
     }
 
-    Connection conn = null;
-
-    // Object of Statement. It is used to create a Statement to execute the query
-    Statement stmt = null;
-
-    //Object of ResultSet => 'It maintains a cursor that points to the current row in the result set'
-    ResultSet resultSet = null;
-    WebDriver driver;
-
-    @BeforeTest
-    public void SetUpConnection() throws SQLException, ClassNotFoundException {
-
-        // Register JDBC driver (JDBC driver name and Database URL)
+    @DataProvider(name = "TestData")
+    public Object[][] SetUpConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
 
-        // Open a connection
         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/121", "root", "");
-
         stmt = conn.createStatement();
         resultSet = stmt.executeQuery("select * from testdata");
 
-        while (resultSet.next()){
-            String temp = resultSet.getString(1);
-            System.out.println(123);
+        Object[][] objects = new Object[3][];
+
+        int i = 0;
+        while (resultSet.next()) {
+            HashMap hashMap = new HashMap();
+            hashMap.put(resultSet.getString(1), resultSet.getString(2));
+            objects[i] = new Object[]{hashMap};
+            i++;
+        }
+        return objects;
+    }
+
+
+    @AfterTest
+    public void CloseTheConnection() throws SQLException {
+
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (Exception ignored) {
+            }
         }
 
-        System.setProperty("webdriver.chrome.driver", "<Path of Driver>\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (Exception ignored) {
+            }
+        }
 
-        // Code to disable the popup of saved password
-        Map<String, Object> prefs = new HashMap<String, Object>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("password_manager_enabled", false);
-        options.setExperimentalOption("prefs", prefs);
-        driver = new ChromeDriver(options);
-        driver.get("<URL>");
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (Exception ignored) {
+            }
+        }
+
     }
 }
